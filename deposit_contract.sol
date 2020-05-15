0f419499ec6795e7657da70b204866c5d02e4f3a
Alex Beregszaszi <alex@rtfs.hu>
2020-05-15 19:03:06 +0100

    Duplicate natspec on contrcat

--- deposit_contract.sol
+++ deposit_contract.sol
@@ -28,8 +28,8 @@ interface IDepositContract {
         bytes32 deposit_data_root
     ) external payable;
 
-    /// @notice Query the current deposit root hash.
-    /// @return The deposit root hash.
+    /// @notice Query the current deposit count.
+    /// @return The deposit count encoded as a little endian 64-bit number.
     function get_deposit_root() external view returns (bytes32);
 
     /// @notice Query the current deposit count.
@@ -50,6 +50,8 @@ interface ERC165 {
 
 // This is a rewrite of the Vyper Eth2.0 deposit contract in Solidity.
 // It tries to stay as close as possible to the original source code.
+/// @notice This is the Ethereum 2.0 deposit contract interface.
+/// For more information see the Phase 0 specification under https://github.com/ethereum/eth2.0-specs
 contract DepositContract is IDepositContract, ERC165 {
     uint constant GWEI = 1e9;
 
@@ -68,6 +70,8 @@ contract DepositContract is IDepositContract, ERC165 {
             zero_hashes[height + 1] = sha256(abi.encodePacked(zero_hashes[height], zero_hashes[height]));
     }
 
+    /// @notice Query the current deposit root hash.
+    /// @return The deposit root hash.
     function get_deposit_root() override external view returns (bytes32) {
         bytes32 node;
         uint size = deposit_count;
@@ -85,10 +89,18 @@ contract DepositContract is IDepositContract, ERC165 {
         ));
     }
 
+    /// @notice Query the current deposit count.
+    /// @return The deposit count encoded as a little endian 64-bit number.
     function get_deposit_count() override external view returns (bytes memory) {
         return to_little_endian_64(uint64(deposit_count));
     }
 
+    /// @notice Submit a Phase 0 DepositData object.
+    /// @param pubkey A BLS12-381 public key.
+    /// @param withdrawal_credentials Commitment to a public key for withdrawals.
+    /// @param signature A BLS12-381 signature.
+    /// @param deposit_data_root The SHA-256 hash of the SSZ-encoded DepositData object.
+    /// Used as a protection against malformed input.
     function deposit(
         bytes calldata pubkey,
         bytes calldata withdrawal_credentials,
