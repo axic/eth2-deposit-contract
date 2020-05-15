aa98581d571c4abf01fde4058caccde6777aa448
Alex Beregszaszi <alex@rtfs.hu>
2020-05-15 12:12:52 +0100

    Revert "Byteswap output"
    This reverts commit 48b7307e562e7c94e63a9dddafa1b8507867a4d1.

--- deposit_contract.sol
+++ deposit_contract.sol
@@ -144,16 +144,16 @@ contract DepositContract is IDepositContract, ERC165 {
     }
 
     function to_little_endian_64(uint64 value) internal pure returns (bytes memory ret) {
+        // Unrolled the loop here.
         ret = new bytes(8);
         bytes8 bytesValue = bytes8(value);
-        // Byteswapping during copying to bytes.
-        ret[0] = bytesValue[7];
-        ret[1] = bytesValue[6];
-        ret[2] = bytesValue[5];
-        ret[3] = bytesValue[4];
-        ret[4] = bytesValue[3];
-        ret[5] = bytesValue[2];
-        ret[6] = bytesValue[1];
-        ret[7] = bytesValue[0];
+        ret[0] = bytesValue[0];
+        ret[1] = bytesValue[1];
+        ret[2] = bytesValue[2];
+        ret[3] = bytesValue[3];
+        ret[4] = bytesValue[4];
+        ret[5] = bytesValue[5];
+        ret[6] = bytesValue[6];
+        ret[7] = bytesValue[7];
     }
 }
